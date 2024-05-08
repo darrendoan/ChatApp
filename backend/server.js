cf3578d4f840c39052a06f977148ac37bdb4223f
DarrenDoan <doandarren95@gmail.com>
2024-05-09 03:56:01 +1000

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 import authRoutes from "./routes/authRoutes.js"
 import messageRoutes from "./routes/messageRoutes.js"
@@ -12,15 +13,21 @@ import { app } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 8000
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // parse the imcoming requests with JSON payloads (from the req.body)
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"/frontend/dist"))); //static middleware
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connection();
